d24b721483185249edc69648c199f814ce6668ef
lfierz <leonie.fierz@sensirion.com>
2021-09-22 13:40:59 +0200

    LKD-410 update for cleaned up interface
    updating commands
    add getting serial number to interface and example
    do measurement loop for 1 minute in example

--- sgp40_i2c_example_usage.c
+++ sgp40_i2c_example_usage.c
@@ -1,7 +1,7 @@
 /*
- * I2C-Generator: 0.2.0
+ * I2C-Generator: 0.3.0
  * Yaml Version: 0.1.0
- * Template Version: local build
+ * Template Version: 0.7.0-62-g3d691f9
  */
 /*
  * Copyright (c) 2021, Sensirion AG
@@ -34,6 +34,7 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <inttypes.h>
 #include <stdio.h>  // printf
 
 #include "sensirion_common.h"
@@ -47,23 +48,36 @@
  */
 
 int main(void) {
+    int16_t error = 0;
+
     // Parameters for deactivated humidity compensation:
     uint16_t default_rh = 0x8000;
     uint16_t default_t = 0x6666;
 
     sensirion_i2c_hal_init();
 
-    // Start Measurement
+    uint16_t serial_number[3];
+    uint8_t serial_number_size = 3;
+    error = sgp40_get_serial_number(serial_number, serial_number_size);
 
-    for (;;) {
-        int error = 0;
+    if (error) {
+        printf("Error executing sgp40_get_serial_number(): %i\n", error);
+    } else {
+        printf("Serial number: %" PRIu64 "\n",
+               (((uint64_t)serial_number[0]) << 32) |
+                   (((uint64_t)serial_number[1]) << 16) |
+                   ((uint64_t)serial_number[2]));
+    }
+
+    // Start Measurement
+    for (int i = 0; i < 60; i++) {
         uint16_t sraw_voc = 0;
 
         sensirion_i2c_hal_sleep_usec(1000000);
 
-        error = sgp40_measure_raw(default_rh, default_t, &sraw_voc);
+        error = sgp40_measure_raw_signal(default_rh, default_t, &sraw_voc);
         if (error) {
-            printf("Error executing sgp40_measure_raw(): "
+            printf("Error executing sgp40_measure_raw_signal(): "
                    "%i\n",
                    error);
         } else {
